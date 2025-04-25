7e1d3953a49e808a67eb99653533198a8e2c05f0
Bommideni Sai Kiran <118157029+saiffuyf@users.noreply.github.com>
2025-04-25 21:03:49 +0530

    Update motiondetector.py

--- motiondetector/detector/motiondetector.py
+++ motiondetector/detector/motiondetector.py
@@ -1,34 +1,3 @@
-# import cv2
-
-# def generate_frames():
-#     cap = cv2.VideoCapture(0)
-#     _, frame1 = cap.read()
-#     _, frame2 = cap.read()
-
-#     while cap.isOpened():
-#         diff = cv2.absdiff(frame1, frame2)
-#         gray = cv2.cvtColor(diff, cv2.COLOR_BGR2GRAY)
-#         blur = cv2.GaussianBlur(gray, (5, 5), 0)
-#         _, thresh = cv2.threshold(blur, 20, 255, cv2.THRESH_BINARY)
-#         dilated = cv2.dilate(thresh, None, iterations=3)
-#         contours, _ = cv2.findContours(dilated, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-
-#         for contour in contours:
-#             if cv2.contourArea(contour) < 500:
-#                 continue
-#             (x, y, w, h) = cv2.boundingRect(contour)
-#             cv2.rectangle(frame1, (x, y), (x+w, y+h), (0, 255, 0), 2)
-
-#         ret, buffer = cv2.imencode('.jpg', frame1)
-#         frame = buffer.tobytes()
-#         yield (b'--frame\r\n'
-#                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
-
-#         frame1 = frame2
-#         ret, frame2 = cap.read()
-
-#         if not ret:
-#             break
 import cv2
 import datetime
 import os
